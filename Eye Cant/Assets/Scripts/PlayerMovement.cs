32bf5a528040d85150d8dae60314f3727c7d1697
Felix Konprecht <felix@konprecht.com>
2019-10-12 16:27:39 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Eye Cant/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller = null;
+    [SerializeField] private Transform cameraTransform = null;
+    [SerializeField] private float movementSpeed = 100;
+    [SerializeField] private float cameraRotationSpeed = 100;
+    
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        LookAround();
+        Move();
+    }
+
+    private void Move()
+    {
+        var input = Vector3.zero;
+
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
+
+        input = transform.TransformDirection(input);
+
+        controller.Move(input * Time.deltaTime * movementSpeed);
+    }
+
+    private void LookAround()
+    {
+        var vertical = Input.GetAxis("Mouse X") * Time.deltaTime * cameraRotationSpeed;
+        var horizontal = Input.GetAxis("Mouse Y") * Time.deltaTime * cameraRotationSpeed;
+
+        transform.Rotate(0, vertical, 0);
+        cameraTransform.Rotate(-horizontal, 0, 0);
+    }
+}
