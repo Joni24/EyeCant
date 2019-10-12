7a25ada25407c8d6a0086daf1d7036446f0d8e44
Felix Konprecht <felix@konprecht.com>
2019-10-12 18:28:01 +0200

    added test Tree and Door Script

---
+++ Eye Cant/Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private GameObject questions = null;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        questions.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        questions.SetActive(false);
+    }
+}
