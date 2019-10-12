1d06379af1b987d89ad3f7b6e7b98cf10517cd36
Felix Konprecht <felix@konprecht.com>
2019-10-12 17:01:37 +0200

    added Player and basic Transition

---
+++ Eye Cant/Assets/Scripts/Transition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public static Transition Instance { get; private set; }
+
+    [SerializeField] private Animation animation = null;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void FadeBlack()
+    {
+        animation.Play("FadeBlack");
+    }
+
+    public void FadeNormal()
+    {
+        animation.Play("FadeNormal");
+    }
+}
